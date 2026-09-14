9dc71d544d315176a997ea8a9cbbb7d0d148a560
Skuryatin Dmitry <dimy34567@gmail.com>
2026-09-14 18:46:55 +0700

    Added input for gender, course, and difficulty.

--- app/src/main/java/com/example/gameofbugs/MainActivity.kt
+++ app/src/main/java/com/example/gameofbugs/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.gameofbugs
 
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +12,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.registration_form)
+
+        val sbDifficulty = findViewById<SeekBar>(R.id.sbDifficulty)
+        val tvDifficultyValue = findViewById<TextView>(R.id.tvDifficultyValue)
+
+        sbDifficulty.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                tvDifficultyValue.text = progress.toString()
+                setGameDifficulty(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) { }
+            override fun onStopTrackingTouch(seekBar: SeekBar?) { }
+        })
+    }
+
+    private fun setGameDifficulty(level: Int) {
     }
 }
